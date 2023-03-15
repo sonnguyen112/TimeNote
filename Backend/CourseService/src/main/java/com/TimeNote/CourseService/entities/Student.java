46afefb69ffb1cfd765703a7240475514f5f06a2
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2023-03-15 20:12:49 +0700

    initTables

---
+++ Backend/CourseService/src/main/java/com/TimeNote/CourseService/entities/Student.java
@@ -0,0 +1,22 @@
+package com.TimeNote.CourseService.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "StudentTable")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long studentID;
+    private String studentName;
+    private String studentCode;
+
+}

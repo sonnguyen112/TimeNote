75abdaaec3a782194e68e9d3f8f9ae9774235370
KhoiRobert <pdkhoi20@apcs.fitus.edu.vn>
2023-05-13 20:14:12 +0700

    khoi

--- Backend/AttendanceService/app/router.py
+++ Backend/AttendanceService/app/router.py
@@ -5,16 +5,26 @@ import json
 from .config import loop, KAFKA_BOOTSTRAP_SERVERS,KAFKA_CONSUMER_GROUP,KAFKA_TOPIC
 from aiokafka import AIOKafkaConsumer
 from fastapi import APIRouter
+from database import cursor
 router = APIRouter()
+
+def add_to_table(student_id,face_encoding):
+    query = ("INSERT INTO faceencode (student_id,face_encoding)\
+             values ({},{});".format(student_id,face_encoding))
+    cursor.execute(query)
+
 
 async def consumer(): 
     consumer = AIOKafkaConsumer(KAFKA_TOPIC, loop= loop, bootstrap_servers= KAFKA_BOOTSTRAP_SERVERS, group_id= KAFKA_CONSUMER_GROUP )
     await consumer.start()
     try:
         async for msg in consumer:
-          
-            print(f"Consumed msg: {msg}")
-         
+            json_load = json.loads(msg.value)
+            json_load_image_byte = json_load['byteArray']
+            json_load_student_code = json_load['studentCode']
+              
+            
+            
     finally:
         await consumer.stop()
 # loop.run_until_complete(consumer())

b56ff8be67e12b9fe13536712e6ca90fcbd15d7f
htrbao <trbaohoo@gmail.com>
2023-05-20 22:47:32 +0700

    baoho

--- Backend/CourseService/src/main/java/com/TimeNote/CourseService/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.TimeNote.CourseService.repository;
-
-import com.TimeNote.CourseService.entities.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}

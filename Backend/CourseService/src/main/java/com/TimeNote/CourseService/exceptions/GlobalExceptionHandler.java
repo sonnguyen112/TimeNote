e96373b7ec86f8a7763cbcfdb6563c3a41f382f5
KhoiRobert <pdkhoi20@apcs.fitus.edu.vn>
2023-05-22 20:28:22 +0700

    newversion

--- Backend/CourseService/src/main/java/com/TimeNote/CourseService/exceptions/GlobalExceptionHandler.java
+++ Backend/CourseService/src/main/java/com/TimeNote/CourseService/exceptions/GlobalExceptionHandler.java
@@ -5,11 +5,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.TimeNote.CourseService.dto.ErrorResponse;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(AppException.class)
-    public ResponseEntity<String> handleAppException(AppException e){
+    public ResponseEntity<ErrorResponse> handleAppException(AppException e){
         e.printStackTrace();
-        return ResponseEntity.status(e.getCode()).body(e.getMessage());
+        return ResponseEntity.status(e.getCode()).body(ErrorResponse.builder().message(e.getMessage()).build());
     }
 }
